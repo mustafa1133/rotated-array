c4b9833d49668127db3a23408f88a4d967e2f2ea
mustafa1133 <44012227+mustafa1133@users.noreply.github.com>
2020-09-28 19:19:43 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+var findMin = function(nums) {
+
+let left = 0;
+let right = nums.length-1;
+
+if (nums.length === 1)
+{
+    return nums[0];
+}
+
+if(nums[left] < nums[right])
+{
+    return nums[left]
+}
+
+while (left <= right){
+    let mid = Math.floor((left + right) /2);
+
+    if(nums[mid] > nums[ mid + 1])
+    {
+        return nums[mid + 1]
+    }
+    else if( nums [mid-1] > nums[mid])
+    {
+        return nums[mid];
+    }
+
+if(nums[mid] > nums[left])
+{
+    left = mid +1;
+}
+else 
+{
+    right = mid -1;
+}
+
+}
+
+
+}
